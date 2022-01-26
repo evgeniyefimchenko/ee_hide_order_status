89880daa198f098f53da12056e006d6b25b04308
Евгений Ефимченко <evgeniy@efimchenko.ru>
2022-01-26 22:40:44 +0300

    functionality added

--- app/addons/ee_hide_order_status/func.php
+++ app/addons/ee_hide_order_status/func.php
@@ -11,3 +11,15 @@ function fn_ee_hide_order_status_install() {
 function fn_ee_hide_order_status_uninstall() {
 	return true;
 }
+
+function fn_ee_hide_order_status_change_status($statuses, $order_status) {
+    $options_array = ['C' => ['D', 'P']];
+    $res = [];
+    if ($options_array[$order_status]) {
+        $options_array[$order_status][] = $order_status;
+        foreach ($options_array[$order_status] as $val) {
+            $res[$val] = $statuses[$val];
+        }
+    }
+    return $res;
+}
